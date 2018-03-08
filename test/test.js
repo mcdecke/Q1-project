4c827334f82070c38176dc6eb523cbce30637879
Mac Decker <mcdecke@gmail.com>
2018-03-08 16:09:55 -0700

    added tests

--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
 const chai = require('chai');
 const expect = chai.expect;
+// const main = require('../js/main.js');
 const main = require('../js/states/GameState.js');
 
 // var SpaceShooter = SpaceShooter || {};
-
 // console.log(localStorage.getItem('High Score'));
 
 describe('SpaceShooter', function () {
   it('is an object', function () {
-    expect(SpaceShooter).to.be.an('object')
+    expect(this).to.be.an('object')
   })
   it('bools will be bools', function(){
     expect(true).to.eql(true)
   })
   it('bools 2: electric boolgaloo', function(){
-    expect(false).to.eql(false)
+    expect(false).to.deep.eql(false)
   })
   it('expects localStorage to exist...', function(){
     expect(localStorage.getItem('High Score')).to.be.less.than(100)
